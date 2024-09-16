20ca93b315e3fa7659edf5fdee4a032258580832
jasonchandler3245643 <chandl12@go.stockton.edu>
2024-09-16 19:32:05 -0400

    TestMeanProgram.java

--- TestMeanProgram.java
+++ TestMeanProgram.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
 
-public class TestMeanProgram {
-
+public class TestMeanProgram
+{
+   
+    
+    public static void main(String[] args) {
+        MeanProgram tester = new MeanProgram();
+    
+        ArrayList<Integer> listOfNumbers = new ArrayList<Integer>();
+        
+        for (int i = 0; i < 100; i++) {
+        
+             listOfNumbers.add(i);
+        
+        }
+        
+        System.out.print(tester.computeMean(listOfNumbers));
+    
+    
+    
+    }
+    
+    
+    
 }
