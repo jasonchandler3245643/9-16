1e33bb5517e1a7c4473ea3ee5961bd774910ace5
jasonchandler3245643 <chandl12@go.stockton.edu>
2024-09-16 19:31:29 -0400

    MeanProgram.java

--- MeanProgram.java
+++ MeanProgram.java
@@ -1,4 +1,43 @@
+//If your IDe produces any comments DELETE ALL OF IT minus class construction
+import java.util.ArrayList;
 
-public class MeanProgram {
+public class MeanProgram
+{
 
+    
+    public double computeMean(ArrayList<Integer> listOfNumbers) {
+    
+    int sum = 0;
+        
+    for (int singleNumber : listOfNumbers) {
+        
+        sum = sum + singleNumber;
+        
+    }        
+    
+    return sum / (double) listOfNumbers.size();
+    
+    }
+    
+    public double computeMode() {
+    // most common
+    int mode = 0;
+    
+        
+    return 0;
+    }
+    
+    public double computeMedian() {
+    // middle when sorted
+    
+    return 0;
+    }
+    
+    public double computerStandardDeviation() {
+    
+    
+    return 0;
+    }
+    
+    
 }
